533bc4a1c0b687c60ea2cd983fd3db8a29368a5d
Daniel Fernández Villanueva <danielfervilus@gmail.com>
2014-08-19 16:40:53 +0200

    Add Lots.

---
+++ src/Lot.cpp
@@ -0,0 +1,24 @@
+/*
+ * Lot.cpp
+ *
+ *  Created on: Jul 3, 2014
+ *      Author: daniel
+ */
+
+#include "Lot.h"
+
+namespace dfv
+{
+
+Lot::Lot()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+Lot::~Lot()
+{
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace dfv */
---
+++ src/Lot.h
@@ -0,0 +1,22 @@
+/*
+ * Lot.h
+ *
+ *  Created on: Jul 3, 2014
+ *      Author: daniel
+ */
+
+#ifndef LOT_H_
+#define LOT_H_
+
+namespace dfv
+{
+
+class Lot
+{
+public:
+	Lot();
+	virtual ~Lot();
+};
+
+} /* namespace dfv */
+#endif /* LOT_H_ */
